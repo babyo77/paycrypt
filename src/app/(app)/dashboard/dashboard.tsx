401822ec496c855ccc5fe803b81b26340736c901
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-05-05 00:28:21 +0530

    Refactor Dashboard component to improve demo link functionality by wrapping the button in a Link component. Update button structure for better clarity and maintainability.

--- src/app/(app)/dashboard/dashboard.tsx
+++ src/app/(app)/dashboard/dashboard.tsx
@@ -71,27 +71,32 @@ export default function Dashboard() {
                       Create
                     </Button>
                   </Link>
-                  <Button
-                    variant="ghost"
-                    size="sm"
-                    className="flex items-center text-xs"
+                  <Link
+                    target="_blank"
+                    href={process.env.NEXT_PUBLIC_PAYMENT_LINKS_DEMO_URL!}
                   >
-                    View demo
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="14"
-                      height="14"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="ml-1"
+                    <Button
+                      variant="ghost"
+                      size="sm"
+                      className="flex items-center text-xs"
                     >
-                      <polyline points="9 18 15 12 9 6"></polyline>
-                    </svg>
-                  </Button>
+                      View demo
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="14"
+                        height="14"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="ml-1"
+                      >
+                        <polyline points="9 18 15 12 9 6"></polyline>
+                      </svg>
+                    </Button>
+                  </Link>
                 </CardFooter>
               </Card>
 
