d4a0f23bb522bec5e48bb4175d7d54f7d3830e7f
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-05-06 10:20:40 +0530

    Update Playground component button text to reflect waitlist status: Change button label to "Will be notified" for users on the waitlist, enhancing user feedback.

--- src/app/(app)/integrations/playground.tsx
+++ src/app/(app)/integrations/playground.tsx
@@ -87,7 +87,7 @@ export default function Playground() {
             size="lg"
             variant={"secondary"}
           >
-            Get Notified
+            {userData?.is_waitlist ? "Will be notified" : "Get Notified"}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
