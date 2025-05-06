88c8a3f9e50a0beebd7a6ed39eea433af5776ec8
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-05-07 01:43:39 +0530

    Correct status string in PayoutPage component: Update 'partially-completed' to 'partially-confirmed' for consistency in status handling.

--- src/app/(app)/payout/payout.tsx
+++ src/app/(app)/payout/payout.tsx
@@ -655,7 +655,7 @@ function PayoutPage() {
                                           "processing"
                                       ? "secondary"
                                       : payout.status.toLowerCase() ===
-                                        "partially-completed"
+                                        "partially-confirmed"
                                       ? "secondary"
                                       : "destructive"
                                   }
